abecad6da7ceeec8167c47108fc242e1af515c7f
Selamanse <selamanse@scheinfrei.info>
2023-10-11 19:58:52 +0200

    Add generic api config

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -4,6 +4,6 @@ import { DeepshoreChatRequest, DeepshoreChatResponse } from 'src/types/chat';
 export async function sendQuestion(
   data: DeepshoreChatRequest
 ): Promise<DeepshoreChatResponse> {
-  const response = await api.post('http://0.0.0.0:8080/chatbot', data);
+  const response = await api.post('/chatbot', data);
   return response.data;
 }
